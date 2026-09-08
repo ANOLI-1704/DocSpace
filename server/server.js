a9a0c8efd1437c74664be325533cee50ef819e0a
Anoli <anoli4050@gmail.com>
2026-09-08 23:26:59 +0530

    Add user authentication

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const connectDB = require("./db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -7,9 +10,13 @@ const PORT = 5000;
 
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Welcome to DocSpace API");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`DocSpace server running on http://localhost:${PORT}`);
